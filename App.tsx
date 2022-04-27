4a2b213f2ab3473b2521df0e8c27c53eaab42a2c
gaurav-11018 <79459872+gaurav-11018@users.noreply.github.com>
2022-04-27 23:53:08 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+let name: string;
+let age: number;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+
+role = [5, "rose"];
+
+
+function App() {
+  return (
+    <div className="App">
+      "Hello World"
+
+    </div>
+  );
+}
+
+export default App;
